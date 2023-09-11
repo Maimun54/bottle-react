cf9d244d618d11a22d1fef692a3304962f897e1e
Maimun54 <maimunislam@gmail.com>
2023-09-11 11:00:25 +0600

    Memorable bottle

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+import Bottles from './components/Bottles/Bottles'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h2>Memorable Water Bottle</h2>
+      <Header></Header>
+      <Bottles></Bottles>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+          <h3>RFL Water Bottle</h3>  
+        </div>
+    );
+};
+
+export default Header;
